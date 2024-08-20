0744de54c1e2546cce6fbbce0d0e0e16a62414e5
Ranjani00 <ranjanit.bcs23@rathinam.in>
2024-08-21 00:15:57 +0530

    Add files via upload

---
+++ OrdderEven/Oddoreven.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Oddoreven{
+ public static void main(String[] args){
+  Scanner scan = new Scanner(System.in);
+   int number = scan.nextInt();
+ if(number%2==0)
+ System.out.println(number+" is even");
+else
+ System.out.println(number+" is odd");
+}}
